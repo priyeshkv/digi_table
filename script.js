d204a1080c2237c1a2f27cfacfaeadf4d2f28740
priyeshkv <priyeshkv@gmail.com>
2025-06-13 15:45:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // sheetID you can find in the URL of your spreadsheet after "spreadsheet/d/"
-const sheetId = "1iUWlbfIS0ZGcl1aL5G5xTN-xaCm1hqCPVJRvSn4LDQ0";
+const sheetId = "1E55ikahFjBCWT2CDFH9qX3sSOflFz8ziids97UKMGjI";
 // sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
 const sheetName = encodeURIComponent("Sheet1");
 const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
